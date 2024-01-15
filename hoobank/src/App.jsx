2fea3408d112e47185af97dd607bfea0b92700ec
codnrawbwj <codnrawbwj@gmail.com>
2024-01-15 14:52:57 -0600

    components setup

--- hoobank/src/App.jsx
+++ hoobank/src/App.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import styles from './style';
+import { Billing, Business, Button, CardDeal, Clients, CTA, FeedbackCard, Footer, GetStarted, Hero, Navbar, Stats, Testimonials } from './components'
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      App
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      
+      <div className={`${styles.flexStart} bg-primary`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`${styles.flexStart} bg-primary ${styles.paddingX}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   )
 }
