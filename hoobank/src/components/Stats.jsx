e4e6d0c51d2912d16d455ba50ec30e66d3e57bad
codnrawbwj <codnrawbwj@gmail.com>
2024-01-16 14:27:16 -0600

    setup 'stats' compoenet

--- hoobank/src/components/Stats.jsx
+++ hoobank/src/components/Stats.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { stats } from '../constants/index'
 
 const Stats = () => {
+  
   return (
-    <div>Stats</div>
+    <section className='flex w-full justify-evenly items-center mb-6 flex-wrap'>
+      {stats.map((stat) => (
+        <div key={stat.id} className='flex-1 flex gap-[8px] font-poppins justify-center items-center'>
+          <h4 className='text-white font-semibold text-[36px]'>{stat.value}{" "} </h4>
+          <p className='text-gradient font-normal text-[15px]'>{stat.title.toUpperCase()}</p>
+        </div>
+      ))}
+    </section>
   )
 }
-
+  
 export default Stats
