55ca2e60285d2782214a25b988e0168cd1e59927
codnrawbwj <codnrawbwj@gmail.com>
2024-01-19 14:21:04 -0600

    set up Clients component

--- hoobank/src/components/Clients.jsx
+++ hoobank/src/components/Clients.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { clients } from '../constants'
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <div className='w-[90%] flex items-center justify-between mx-auto'>
+    {clients.map((client) => (
+      <img key={client.id} src={client.logo} alt="client logo" className={`max-w-[192.25px]`} />
+    ))}
+  </div>
+)
 
 export default Clients
