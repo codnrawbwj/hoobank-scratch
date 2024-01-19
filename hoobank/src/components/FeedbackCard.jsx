4e70813c0cc0c277020abfe8ac3eb4df0abdcc74
codnrawbwj <codnrawbwj@gmail.com>
2024-01-19 14:09:58 -0600

    set up Testimonials componenet

--- hoobank/src/components/FeedbackCard.jsx
+++ hoobank/src/components/FeedbackCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { quotes } from '../assets'
 
-const FeedbackCard = () => {
-  return (
-    <div>FeedbackCard</div>
-  )
-}
+const FeedbackCard = ({ id, content, name, title, img}) => (
+  <div className='max-w-[370px] h-[395px] feedback-card rounded-[20px] flex-1 font-poppins py-12 px-8'>
+    <img  src={ quotes } alt="quotes" />
+    <p className='mt-[56px] text-[18px] text-white leading-[32px]'>{content}</p>
+    <div className='mt-[48px] flex justify-start items-center'>
+      <img src={img} alt="feeback user image" className='w-[48px]' />
+      <div className='flex flex-col justify-center ml-4'>
+        <h2 className='text-white text-[18px] font-semibold'>{name}</h2>
+        <p className='text-dimWhite'>{title}</p>
+      </div>
+    </div>
+  </div>
+)
 
 export default FeedbackCard
--- hoobank/src/components/Testimonials.jsx
+++ hoobank/src/components/Testimonials.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import FeedbackCard from './FeedbackCard'
+import { feedbacks } from '../constants'
+import styles from '../style'
 
-const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
-}
+const Testimonials = () => (
+  <section id='clients' className='my-[120px]'>
+    <div className={`text-white font-poppins flex justify-start items-center py-8`}>
+      <div className='max-w-[470px] font-semibold text-[48px] leading-[81.6px] mr-[230px]'>
+        <h1>What people are saying about us</h1>
+      </div>
+      <div className='max-w-[450px] font-normal text-[18px] leading-[32.4px] text-dimWhite'>
+        <p>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+    </div>
+    <div className={`${styles.flexStart} gap-[70px] mt-10`}>
+      {feedbacks.map((feedback, id) => (
+        <FeedbackCard key={id} {...feedback} />
+      ))}
+    </div>
+  </section>
+)
 
 export default Testimonials
